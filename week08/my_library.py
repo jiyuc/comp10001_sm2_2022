dd2e5336c506271945dcfc889aaa9fd24331a9d2
Freddie <chen3645935@hotmail.com>
2022-10-13 16:27:28 +1100

    upload till week11

---
+++ week08/my_library.py
@@ -0,0 +1,17 @@
+AGE = 18
+LOCATION = "Melbourne"
+
+class first_cls:
+    def __init__(self):
+        print("first!")
+        return
+
+class second_cls:
+    def __init__(self):
+        print("second!")
+        return
+    
+class third_cls:
+    def __init__(self, param):
+        print("third!", param)
+        return
